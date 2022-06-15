55f3e19ae428e5a20947132694071dbb58691b84
MarkSFrancis <mrkfrancis8@gmail.com>
2022-06-15 22:50:12 +0100

    Fixed values for bitrate quality detection

--- utils/stream/quality/getAudioSrcQuality.ts
+++ utils/stream/quality/getAudioSrcQuality.ts
@@ -2,11 +2,11 @@ import { MediaSource } from "../../trackTypes";
 import { QualityMode } from "./qualityMode";
 
 export const getAudioSrcQuality = (source: MediaSource): QualityMode => {
-  if (source.Bitrate > 400) {
+  if (source.Bitrate > 400000) {
     return "ORIGINAL_QUALITY";
-  } else if (source.Bitrate > 300) {
+  } else if (source.Bitrate > 300000) {
     return "HIGH_QUALITY";
-  } else if (source.Bitrate > 200) {
+  } else if (source.Bitrate > 200000) {
     return "MEDIUM_QUALITY";
   } else {
     return "LOW_QUALITY";
