da33787475d23673776284a1d17d5e24a98435bf
Mark Francis <mark@moonstar.ai>
2024-03-11 12:53:05 +0000

    Update to new eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+    "next/core-web-vitals",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ["*.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
+      },
+    },
+    {
+      files: ["*.test.ts"],
+      rules: {
+        "@typescript-eslint/no-non-null-assertion": 0,
+      },
+    },
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "react"],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
